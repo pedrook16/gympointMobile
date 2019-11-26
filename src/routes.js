215b9532147f13238ac11917844e9c529d89cce4
Pedro Henrique <pedrohenrique@iMac-de-Pedro.local>
2019-11-26 15:28:06 -0300

    redux

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {Image} from 'react-native';
+
+import logo from '~/assets/logo-inline.png';
 
 import SignIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
@@ -9,7 +13,15 @@ export default (isSined = false) =>
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({SignIn}),
-        App: createBottomTabNavigator({Dashboard}),
+        App: createBottomTabNavigator(
+          {Dashboard},
+          {
+            defaultNavigationOptions: {},
+            navigationOptions: {
+              tabBarLabel: <Image source={logo} />,
+            },
+          },
+        ),
       },
       {
         initialRouteName: isSined ? 'App' : 'Sign',
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,26 @@
+import {takeLatest, put, all} from 'redux-saga/effects';
+import {Alert} from 'react-native';
+
+import {signInSuccess, signFailure} from './actions';
+
+export function* signIn({payload}) {
+  try {
+    const {id} = payload;
+
+    yield put(signInSuccess(id));
+
+    // history.push('/student');
+  } catch (err) {
+    Alert.alert('Falha na autenticação', 'Verifique seu usuário');
+    yield put(signFailure());
+  }
+}
+
+export function signOut() {
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
